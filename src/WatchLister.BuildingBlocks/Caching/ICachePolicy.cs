e389f6cf90d9449388f19095edca758d80e8f406
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2023-03-24 14:23:53 +0330

    feat: define cache policy with absolute expiration time

---
+++ src/WatchLister.BuildingBlocks/Caching/ICachePolicy.cs
@@ -0,0 +1,16 @@
+﻿namespace WatchLister.BuildingBlocks.Caching;
+
+public interface ICachePolicy<in TRequest, TResponse> where TRequest : IRequest<TResponse>
+{
+    DateTime? AbsoluteExpirationRelativeToNow { get; }
+
+    string GetCacheKey(TRequest request)
+    {
+        var obj = new { Request = request };
+        var properties = obj.Request
+            .GetType()
+            .GetProperties()
+            .Select(x => $"{x.Name}:{x.GetValue(obj.Request, null)}");
+        return $"{typeof(TRequest).FullName}{{{string.Join(",", properties)}}}";
+    }
+}
