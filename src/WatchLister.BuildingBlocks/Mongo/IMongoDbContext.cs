23b462341a39d2c720d3c3dcb120f8e809b72549
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2023-03-24 17:11:20 +0330

    feat: mongo-db context interface & implementation

---
+++ src/WatchLister.BuildingBlocks/Mongo/IMongoDbContext.cs
@@ -0,0 +1,11 @@
+﻿namespace WatchLister.BuildingBlocks.Mongo;
+
+public interface IMongoDbContext : IDisposable
+{
+    IMongoDatabase Database { get; }
+    IMongoClient MongoClient { get; }
+    Task BeginTransactionAsync();
+    Task RollbackTransactionAsync();
+    Task CommitTransactionAsync();
+    IMongoCollection<T> GetCollection<T>();
+}
---
+++ src/WatchLister.BuildingBlocks/Mongo/MongoDbContext.cs
@@ -0,0 +1,58 @@
+﻿namespace WatchLister.BuildingBlocks.Mongo;
+
+public class MongoDbContext : IMongoDbContext
+{
+    public MongoDbContext(MongoOptions options)
+    {
+        RegisterConventions();
+
+        MongoClient = new MongoClient(options.ConnectionString);
+        Database = MongoClient.GetDatabase(options.DatabaseName);
+    }
+
+    public IClientSessionHandle Session { get; set; } = null!;
+    public IMongoDatabase Database { get; set; }
+    public IMongoClient MongoClient { get; set; }
+
+    public async Task BeginTransactionAsync()
+    {
+        Session = await MongoClient.StartSessionAsync();
+        Session.StartTransaction();
+    }
+
+    public async Task RollbackTransactionAsync() => await Session.AbortTransactionAsync();
+
+    public async Task CommitTransactionAsync()
+    {
+        if (Session.IsInTransaction)
+        {
+            await Session.CommitTransactionAsync();
+        }
+
+        Session.Dispose();
+    }
+
+    public IMongoCollection<T> GetCollection<T>() => Database.GetCollection<T>(typeof(T).Name.ToLower());
+
+    public void Dispose()
+    {
+        while (Session is { IsInTransaction: true })
+        {
+            Thread.Sleep(TimeSpan.FromMilliseconds(100));
+        }
+
+        GC.SuppressFinalize(this);
+    }
+
+    private static void RegisterConventions()
+    {
+        ConventionRegistry.Register("conventions", new ConventionPack
+        {
+            new CamelCaseElementNameConvention(),
+            new IgnoreExtraElementsConvention(true),
+            new EnumRepresentationConvention(BsonType.String),
+            new IgnoreIfDefaultConvention(true),
+            new ImmutablePocoConvention(),
+        }, _ => true);
+    }
+}
