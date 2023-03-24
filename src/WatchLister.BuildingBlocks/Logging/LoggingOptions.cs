4afeb42f51bdf04cb1f7686c06080db280abe555
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2023-03-24 14:30:52 +0330

    feat: logging options setting

---
+++ src/WatchLister.BuildingBlocks/Logging/LoggingOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace WatchLister.BuildingBlocks.Logging;
+
+public class LoggingOptions
+{
+    public string? Level { get; set; }
+    public string? SeqUrl { get; set; }
+    public string? ElasticSearch { get; set; }
+    public string? LogTemplate { get; set; }
+    public string? LogPath { get; set; }
+}
