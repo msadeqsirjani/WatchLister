080a873aa6298d4c50fc7158a15139b810d9227a
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2023-03-24 14:26:52 +0330

    feat: apply DI for caching strategies

---
+++ src/WatchLister.BuildingBlocks/Caching/CachingExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace WatchLister.BuildingBlocks.Caching;
+
+public static class CachingExtensions
+{
+    public static IServiceCollection AddCachingRequestPolicies(this IServiceCollection services, IList<Assembly>? assemblies)
+    {
+        services.Scan(scan => scan
+            .FromAssemblies(assemblies ?? AppDomain.CurrentDomain.GetAssemblies())
+            .AddClasses(classes => classes.AssignableTo(typeof(ICachePolicy<,>)), false)
+            .AsImplementedInterfaces()
+            .WithTransientLifetime());
+
+        services.Scan(scan => scan
+            .FromAssemblies(assemblies ?? AppDomain.CurrentDomain.GetAssemblies())
+            .AddClasses(classes => classes.AssignableTo(typeof(IInvalidateCachePolicy<,>)),
+                false)
+            .AsImplementedInterfaces()
+            .WithTransientLifetime());
+
+        return services;
+    }
+}
