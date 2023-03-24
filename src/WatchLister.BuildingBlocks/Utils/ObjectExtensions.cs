1a906115c1582a1198908caf85d740e5a78abbd6
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2023-03-24 14:29:43 +0330

    feat: prepare query string using reflections

---
+++ src/WatchLister.BuildingBlocks/Utils/ObjectExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace WatchLister.BuildingBlocks.Utils;
+
+public static class ObjectExtensions
+{
+    public static string GetQueryString(this object obj)
+    {
+        var properties = from property in obj.GetType().GetProperties()
+                         where property.GetValue(obj, null) != null
+                         select $"{property.Name}-{HttpUtility.UrlEncode(property.GetValue(obj, null).ToString())}";
+
+        return string.Join("&", properties.ToArray());
+    }
+}
