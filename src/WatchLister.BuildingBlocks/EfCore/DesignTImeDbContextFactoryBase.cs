3024a2ebe1430e4ceca4b0f43fa1b331af0f56e5
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2023-03-24 14:33:35 +0330

    feat: prepare & define db-context factory using all configuration like, appsettings, env vars, ...

---
+++ src/WatchLister.BuildingBlocks/EfCore/DesignTImeDbContextFactoryBase.cs
@@ -0,0 +1,57 @@
+﻿namespace WatchLister.BuildingBlocks.EfCore;
+
+public abstract class DesignTImeDbContextFactoryBase<TContext> : IDesignTimeDbContextFactory<TContext> where TContext : DbContext
+{
+    public TContext CreateDbContext(string[] args)
+    {
+        var path = Directory.GetCurrentDirectory();
+        var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        return Create(path, env);
+    }
+
+    protected abstract TContext CreateNewInstance(DbContextOptions<TContext> options);
+
+    public TContext Create()
+    {
+        var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        var path = AppContext.BaseDirectory;
+        return Create(path, env);
+    }
+
+    public TContext Create(string path, string? env)
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(path)
+            .AddJsonFile("appsettings.json")
+            .AddJsonFile($"appsettings.{env}.json", true)
+            .AddEnvironmentVariables();
+
+        var config = builder.Build();
+        var connectionString = config.GetConnectionString("DefaultConnection");
+
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            throw new InvalidOperationException("Could not find a connection string named 'Default'");
+        }
+
+        return Create(connectionString);
+    }
+
+    public TContext Create(string connectionString)
+    {
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            throw new ArgumentException($"{nameof(connectionString)} is null or empty", nameof(connectionString));
+        }
+
+        var optionsBuilder = new DbContextOptionsBuilder<TContext>();
+
+        Console.WriteLine("DesignTimeDbContextFactory.Create(string): Connection String: {0}", connectionString);
+
+        optionsBuilder.UseSqlServer(connectionString);
+
+        var options = optionsBuilder.Options;
+
+        return CreateNewInstance(options);
+    }
+}
