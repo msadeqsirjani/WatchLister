dd89392b1e12014bdb27ea800ae7bf60e03681b1
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2023-03-24 14:29:11 +0330

    feat: prepare method like Select return item with its index

---
+++ src/WatchLister.BuildingBlocks/Utils/EnumerableExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace WatchLister.BuildingBlocks.Utils;
+
+public static class EnumerableExtensions
+{
+    public static IEnumerable<(T item, int index)> WithIndex<T>(this IEnumerable<T> source) =>
+        source.Select((item, index) => (item, index));
+}
