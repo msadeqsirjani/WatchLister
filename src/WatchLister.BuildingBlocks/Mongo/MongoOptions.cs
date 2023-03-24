6a295e35bfbeda97c8a4b98ab04b044dec19afd6
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2023-03-24 17:11:35 +0330

    feat: mongo-db options setting

---
+++ src/WatchLister.BuildingBlocks/Mongo/MongoOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace WatchLister.BuildingBlocks.Mongo;
+
+public class MongoOptions
+{
+    public string ConnectionString { get; set; } = string.Empty;
+    public string DatabaseName { get; set; } = string.Empty;
+    public static Guid UniqueId { get; set; } = Guid.NewGuid();
+}
