563ab36366a44734b7e592920094737327cfd191
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2023-03-24 14:19:31 +0330

    feat: prepare functionalities for calling cache key

---
+++ src/WatchLister.BuildingBlocks/Caching/CacheKey.cs
@@ -0,0 +1,7 @@
+﻿namespace WatchLister.BuildingBlocks.Caching;
+
+public static class CacheKey
+{
+    public static string With(params string[] keys) => string.Join(",", keys);
+    public static string With(Type ownerType, params string[] keys) => With($"{ownerType.GetCacheKey()}:{With(keys)}");
+}
