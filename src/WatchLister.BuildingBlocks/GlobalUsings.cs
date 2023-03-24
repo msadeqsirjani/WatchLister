e4ef95aacb43daaa41e65dc16ae982ca29492b49
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2023-03-24 14:34:55 +0330

    feat: global using directives

---
+++ src/WatchLister.BuildingBlocks/GlobalUsings.cs
@@ -0,0 +1,22 @@
+// Global using directives
+
+global using System.ComponentModel;
+global using System.Diagnostics;
+global using System.Globalization;
+global using System.Reflection;
+global using System.Web;
+global using EasyCaching.Core;
+global using MediatR;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Design;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Logging;
+global using Serilog;
+global using Serilog.Events;
+global using Serilog.Exceptions;
+global using Serilog.Sinks.SpectreConsole;
+global using WatchLister.BuildingBlocks.Domain;
+global using WatchLister.BuildingBlocks.Utils;
