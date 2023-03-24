ff85a69f1ca32c8503a1ae01bab8b33a9a5bf4bc
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2023-03-24 14:24:12 +0330

    feat: define policy for invalidating cached data

---
+++ src/WatchLister.BuildingBlocks/Caching/IInvalidateCachePolicy.cs
@@ -0,0 +1,19 @@
+﻿namespace WatchLister.BuildingBlocks.Caching;
+
+public interface IInvalidateCachePolicy<in TRequest, TResponse> where TRequest : IRequest<TResponse>
+{
+    string GetCacheKey(TRequest request)
+    {
+        var obj = new { Request = request };
+        var properties = obj.Request
+            .GetType()
+            .GetProperties()
+            .Select(x => $"{x.Name}:{x.GetValue(obj.Request, null)}");
+        return $"{typeof(TRequest).FullName}{{{string.Join(",", properties)}}}";
+    }
+}
+
+public interface IInvalidateCachePolicy<in TRequest> : IInvalidateCachePolicy<TRequest, Unit> where TRequest : IRequest<Unit>
+{
+
+}
