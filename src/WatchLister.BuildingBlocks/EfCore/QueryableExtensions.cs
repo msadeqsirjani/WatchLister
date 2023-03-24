e255936ce1ee1c9f4aaa2b49e74da76d7c5be3fc
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2023-03-24 14:34:08 +0330

    feat: some queryable extension methods are defined

---
+++ src/WatchLister.BuildingBlocks/EfCore/QueryableExtensions.cs
@@ -0,0 +1,38 @@
+﻿namespace WatchLister.BuildingBlocks.EfCore;
+
+public static class QueryableExtensions
+{
+    public static async Task<ListResultModel<T>> PaginateAsync<T>(this IQueryable<T> collection, IPageList query)
+        where T : notnull => await collection.PaginateAsync(query.Page, query.Size);
+
+    public static async Task<ListResultModel<T>> PaginateAsync<T>(this IQueryable<T> collection, int page = 1,
+        int size = 10) where T : notnull
+    {
+        if (page <= 0) page = 1;
+
+        if (size <= 0) size = 10;
+
+        var isEmpty = await collection.AnyAsync() == false;
+        if (isEmpty) return ListResultModel<T>.Empty;
+
+        var totalItems = await collection.CountAsync();
+        var totalPages = (int) Math.Ceiling((decimal) totalItems / size);
+        var data = await collection.Limit(page, size).ToListAsync();
+
+        return ListResultModel<T>.Create(data, totalItems, page, size);
+    }
+
+    public static IQueryable<T> Limit<T>(this IQueryable<T> collection, IPageList query) => collection.Limit(query.Page, query.Size);
+
+    public static IQueryable<T> Limit<T>(this IQueryable<T> collection, int page = 1, int size = 10)
+    {
+        if (page <= 0) page = 1;
+
+        if (size <= 0) size = 10;
+
+        var skip = (page - 1) * size;
+        var data = collection.Skip(skip).Take(size);
+
+        return data;
+    }
+}
