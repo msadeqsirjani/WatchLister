dfa0dfd3268bbdbc59d4f12355ec7ada932bd5b4
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2023-03-30 21:38:21 +0330

    feat: define common problem detail class

---
+++ src/WatchLister.BuildingBlocks/Security/ForbiddenProblemDetails.cs
@@ -0,0 +1,12 @@
+﻿namespace WatchLister.BuildingBlocks.Security;
+
+public class ForbiddenProblemDetails : ProblemDetails
+{
+    public ForbiddenProblemDetails(string? details = null)
+    {
+        Title = "Forbidden";
+        Detail = details;
+        Status = 403;
+        Type = "https://httpstatuses.com/403";
+    }
+}
