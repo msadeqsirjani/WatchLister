97da8358e649b3d311be078854c2b2c3f873eeb4
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2023-03-30 20:20:12 +0330

    feat: validation error is defined
    to clarify validation errors on field and custom messages

---
+++ src/WatchLister.BuildingBlocks/Validation/ValidationError.cs
@@ -0,0 +1,13 @@
+﻿namespace WatchLister.BuildingBlocks.Validation;
+
+public class ValidationError
+{
+    public ValidationError(string? field, string message)
+    {
+        Field = field != string.Empty ? field : null;
+        Message = message;
+    }
+
+    public string? Field { get; set; }
+    public string Message { get; set; }
+}
