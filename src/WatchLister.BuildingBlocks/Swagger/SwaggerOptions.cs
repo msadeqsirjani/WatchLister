d15759ea99382b755a32323b92577bf8b77e6802
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2023-03-30 20:38:12 +0330

    feat: swagger options is defined

---
+++ src/WatchLister.BuildingBlocks/Swagger/SwaggerOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace WatchLister.BuildingBlocks.Swagger;
+
+public class SwaggerOptions
+{
+    public string Title { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string Version { get; set; } = string.Empty;
+}
