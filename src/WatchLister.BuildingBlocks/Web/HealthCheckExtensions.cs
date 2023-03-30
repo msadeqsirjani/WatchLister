c96f0c54d8c1b3c6cd6cb92c1c7310dca8d00cb0
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2023-03-30 20:55:45 +0330

    feat: using custom health check ui

---
+++ src/WatchLister.BuildingBlocks/Web/HealthCheckExtensions.cs
@@ -0,0 +1,70 @@
+﻿namespace WatchLister.BuildingBlocks.Web;
+
+public static class HealthCheckExtensions
+{
+    public static IApplicationBuilder UseCustomHealthCheck(this IApplicationBuilder app)
+    {
+        app.UseHealthChecks("/healthz", new HealthCheckOptions
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse,
+                ResultStatusCodes =
+                {
+                    [HealthStatus.Healthy] = StatusCodes.Status200OK,
+                    [HealthStatus.Degraded] = StatusCodes.Status500InternalServerError,
+                    [HealthStatus.Unhealthy] = StatusCodes.Status503ServiceUnavailable
+                }
+            })
+            .UseHealthChecks("/health", new HealthCheckOptions
+            {
+                Predicate = check => !check.Tags.Contains("services"),
+                AllowCachingResponses = false,
+                ResponseWriter = WriteResponseAsync
+            })
+            .UseHealthChecks("/health/ready", new HealthCheckOptions
+            {
+                Predicate = _ => true,
+                AllowCachingResponses = false,
+                ResponseWriter = WriteResponseAsync
+            })
+            .UseHealthChecksUI(setup =>
+            {
+                setup.ApiPath = "/healthcheck";
+                setup.UIPath = "/healthcheck-ui";
+            });
+
+        return app;
+    }
+
+    private static Task WriteResponseAsync(HttpContext context, HealthReport result)
+    {
+        context.Response.ContentType = "application/json; charset=utf-8";
+
+        var options = new JsonWriterOptions
+        {
+            Indented = true
+        };
+
+        using var stream = new MemoryStream();
+        using (var writer = new Utf8JsonWriter(stream, options))
+        {
+            writer.WriteStartObject();
+            writer.WriteString("status", result.Status.ToString());
+            writer.WriteStartObject("results");
+           
+            foreach (var entry in result.Entries)
+            {
+                writer.WriteStartObject(entry.Key);
+                writer.WriteString("status", entry.Value.Status.ToString());
+                writer.WriteEndObject();
+            }
+
+            writer.WriteEndObject();
+            writer.WriteEndObject();
+        }
+
+        var json = Encoding.UTF8.GetString(stream.ToArray());
+
+        return context.Response.WriteAsync(json);
+    }
+}
