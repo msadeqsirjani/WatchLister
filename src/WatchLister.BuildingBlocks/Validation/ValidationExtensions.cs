cfd47a0b7479bb3637d899d681b4bf6ac56ec20a
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2023-03-30 20:22:28 +0330

    feat: converting validation result into custom model & register validator from assembly

---
+++ src/WatchLister.BuildingBlocks/Validation/ValidationExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace WatchLister.BuildingBlocks.Validation;
+
+public static class ValidationExtensions
+{
+    public static async Task HandleValidationAsync<TRequest>(this IValidator<TRequest> validator, TRequest request)
+    {
+        var validationResult = await validator.ValidateAsync(request);
+
+        if (!validationResult.IsValid)
+        {
+            throw new ValidationException(validationResult.ToValidationResultModel());
+        }
+    }
+
+    public static IServiceCollection AddCustomValidators(this IServiceCollection services, Assembly assembly)
+    {
+        return services.AddValidatorsFromAssembly(assembly);
+    }
+
+    private static ValidationResultModel ToValidationResultModel(this ValidationResult validationResult) => new(validationResult);
+}
