681521724a3f375565fc7c8629c94cfde1198015
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2023-03-30 20:21:18 +0330

    feat: validation result model created
    using this class containing all gained validation errors

---
+++ src/WatchLister.BuildingBlocks/Validation/ValidationResultModel.cs
@@ -0,0 +1,17 @@
+﻿namespace WatchLister.BuildingBlocks.Validation;
+
+public class ValidationResultModel
+{
+    public ValidationResultModel(ValidationResult validationResult)
+    {
+        Errors = validationResult.Errors
+            .Select(error => new ValidationError(error.PropertyName, error.ErrorMessage))
+            .ToList();
+    }
+
+    public int StatusCode { get; set; } = (int)HttpStatusCode.BadRequest;
+    public string Message { get; set; } = "Validation Failed";
+    public List<ValidationError> Errors { get; }
+
+    public override string ToString() => JsonSerializer.Serialize(this);
+}
