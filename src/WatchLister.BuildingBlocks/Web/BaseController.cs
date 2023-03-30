036523e14e85ffad19cda47953fb7a94b104123a
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2023-03-30 20:54:12 +0330

    feat: define base controller

---
+++ src/WatchLister.BuildingBlocks/Web/BaseController.cs
@@ -0,0 +1,10 @@
+﻿namespace WatchLister.BuildingBlocks.Web;
+
+[ApiController]
+public class BaseController : Controller
+{
+    protected const string BaseApiPath = "api/v{version:apiVersion}";
+
+    private IMediator? _mediator;
+    protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<IMediator>();
+}
