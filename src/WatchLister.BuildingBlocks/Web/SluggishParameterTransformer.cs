c28faaeb5c4fe6f8790a3096acc895806abfa16a
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2023-03-30 20:54:59 +0330

    feat: define sluggish parameter transformer to make clean url

---
+++ src/WatchLister.BuildingBlocks/Web/SluggishParameterTransformer.cs
@@ -0,0 +1,12 @@
+﻿namespace WatchLister.BuildingBlocks.Web;
+
+
+public class SluggishParameterTransformer : IOutboundParameterTransformer
+{
+    public string? TransformOutbound(object? value)
+    {
+        return value == null
+            ? null
+            : Regex.Replace(value.ToString() ?? string.Empty, "([a-z])([A-Z])", "$1-$2").ToLower();
+    }
+}
