cb341331b9df68eae7c3cd08fcb7d8e7510d5886
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2023-03-30 20:53:56 +0330

    feat: define an interface for paging request

---
+++ src/WatchLister.BuildingBlocks/Web/IPageRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace WatchLister.BuildingBlocks.Web;
+
+public interface IPageRequest
+{
+    public List<string> Includes { get; set; }
+    public List<FilterModel> Filters { get; set; }
+    public List<string> Sorts { get; set; }
+    public int Page { get; set; }
+    public int Size { get; set; }
+}
