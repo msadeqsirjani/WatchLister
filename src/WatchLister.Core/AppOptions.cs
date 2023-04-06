e7d7291fb8da17b860e5bc81cc7f6c8cc743e58a
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2023-04-06 20:08:32 +0330

    Add AppOptions class and Google.Apis.YouTube.v3 package reference to WatchLister.Core project

---
+++ src/WatchLister.Core/AppOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace WatchLister.Core;
+
+public class AppOptions
+{
+    public string Name { get; set; } = string.Empty;
+    public string ApiAddress { get; set; } = string.Empty;
+    public string Instance { get; set; } = string.Empty;
+    public string Version { get; set; } = string.Empty;
+    public bool DisplayBanner { get; set; } = true;
+    public bool DisplayVersion { get; set; } = true;
+    public string Description { get; set; } = string.Empty;
+}
