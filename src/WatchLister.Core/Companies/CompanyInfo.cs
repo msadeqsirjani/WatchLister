9a5b46b0b0181110f5f77bf4e05f346ea0562506
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2023-04-07 20:19:45 +0330

    Add CompanyInfo class to WatchLister.Core/Companies

---
+++ src/WatchLister.Core/Companies/CompanyInfo.cs
@@ -0,0 +1,41 @@
+namespace WatchLister.Core.Companies;
+
+public class CompanyInfo : IEqualityComparer<CompanyInfo>
+{
+    public CompanyInfo(int id, string name, string? logoPath = null)
+    {
+        Id = id;
+        Name = name;
+        LogoPath = logoPath;
+    }
+
+    public int Id { get; init; }
+    public string Name { get; init; }
+    public string? LogoPath { get; init; }
+
+    public bool Equals(CompanyInfo? x, CompanyInfo? y)
+    {
+        return x != null &&
+               y != null &&
+               x.Id == y.Id &&
+               x.Name == y.Name &&
+               x.LogoPath == y.LogoPath;
+    }
+
+    public int GetHashCode(CompanyInfo obj)
+    {
+        unchecked
+        {
+            var hash = 17;
+            hash = hash * 23 + obj.Id.GetHashCode();
+            hash = hash * 23 + obj.Name.GetHashCode();
+            return hash;
+        }
+    }
+
+    public override bool Equals(object? obj) => obj is CompanyInfo info && Equals(this, info);
+
+    public override int GetHashCode() => GetHashCode(this);
+
+    public override string ToString() => string.IsNullOrWhiteSpace(Name) ? "n/a" : $"{Name} ({Id})";
+}
