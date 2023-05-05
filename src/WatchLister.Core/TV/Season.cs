0e265ed6c70b71993ee6e314f82f723480f041e3
Mohammad Sadeq Sirjani <m.sadeq.sirjani@gmail.com>
2023-05-05 19:42:10 +0330

    feat(core-season): add season class

---
+++ src/WatchLister.Core/TV/Season.cs
@@ -0,0 +1,13 @@
+﻿namespace WatchLister.Core.TV;
+
+public class Season
+{
+    public int Id { get; init; }
+    public DateTime? AirDate { get; init; }
+    public int EpisodeCount { get; init; }
+    public string PosterPath { get; init; } = string.Empty;
+    public string Overview { get; init; }= string.Empty;
+    public int SeasonNumber { get; init; }
+
+    public override string ToString() => $"({SeasonNumber} - {AirDate:yyyy-MM-dd})";
+}
