e3244c01bc6d9d32761a60bef49656949a34dfae
Mohammad Sadeq Sirjani <m.sadeq.sirjani@gmail.com>
2023-05-05 18:35:12 +0330

    feat(core-companies): add ParentCompany class

--- src/WatchLister.Core/Companies/ParentCompany.cs
+++ src/WatchLister.Core/Companies/ParentCompany.cs
@@ -1,10 +1,10 @@
-namespace WatchLister.Core.Companies;
+﻿namespace WatchLister.Core.Companies;
 
 public class ParentCompany
 {
     public int Id { get; init; }
-    public string Name { get; set; } = string.Empty;
-    public string LogoPath { get; set; } = string.Empty;
+    public string Name { get; init; } = string.Empty;
+    public string? LogoPath { get; init; }
 
-    public override string ToString() => string.IsNullOrEmpty(Name) ? "n/a" : $"{Name} ({Id})";
+    public override string ToString() => string.IsNullOrWhiteSpace(Name) ? "n/a" : $"{Name} ({Id})";
 }
