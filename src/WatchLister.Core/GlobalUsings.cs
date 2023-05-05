96dad35bf497ceff1d25342be8535f1bd61cce1f
Mohammad Sadeq Sirjani <m.sadeq.sirjani@gmail.com>
2023-05-05 19:45:10 +0330

    Create GlobalUsings.cs

---
+++ src/WatchLister.Core/GlobalUsings.cs
@@ -0,0 +1,11 @@
+// Global using directives
+
+global using System.Reflection;
+global using Newtonsoft.Json;
+global using Newtonsoft.Json.Linq;
+global using WatchLister.Core.Collections;
+global using WatchLister.Core.Companies;
+global using WatchLister.Core.Generals;
+global using WatchLister.Core.Genres;
+global using WatchLister.Core.Keywords;
+global using WatchLister.Core.People;
