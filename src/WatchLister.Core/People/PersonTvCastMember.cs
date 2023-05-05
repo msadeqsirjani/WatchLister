9e4548af025a12af9dc79f7e53d7fb5e8d356b33
Mohammad Sadeq Sirjani <m.sadeq.sirjani@gmail.com>
2023-05-05 19:07:17 +0330

    Update PersonTvCastMember.cs

--- src/WatchLister.Core/People/PersonTvCastMember.cs
+++ src/WatchLister.Core/People/PersonTvCastMember.cs
@@ -5,7 +5,7 @@ public abstract class PersonTvCastMember
     public int Id { get; init; }
     public string Character { get; init; } = string.Empty;
     public string CreditId { get; init; } = string.Empty;
-    public int EpisodeCount { get; init; } = string.Empty;
+    public int EpisodeCount { get; init; }
     public DateTime FirstAirDate { get; init; }
     public string Name { get; init; } = string.Empty;
     public string OriginalName { get; init; } = string.Empty;
