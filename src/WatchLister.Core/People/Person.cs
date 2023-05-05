271c674c7688fd265d37ed887864dba15ae8815d
Mohammad Sadeq Sirjani <m.sadeq.sirjani@gmail.com>
2023-05-05 19:02:56 +0330

    refactor(core-person): update person class

--- src/WatchLister.Core/People/Person.cs
+++ src/WatchLister.Core/People/Person.cs
@@ -1,26 +1,23 @@
-namespace WatchLister.Core.People;
+﻿namespace WatchLister.Core.People;
 
 public class Person
 {
-    public Person()
-    {
-        AlsoKnownAs = new List<string>();
-    }
+    public Person() => AlsoKnownAs = Array.Empty<string>();
 
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public IReadOnlyCollection<string> AlsoKnownAs { get; set; }
-    public string? KnownForDepartment { get; set; }
-    public bool Audit { get; set; }
-    public string? Biography { get; set; }
-    public DateTime? BirthDay { get; set; }
-    public DateTime? DeathDay { get; set; }
-    public Genre Genre { get; set; }
-    public string? HomePage { get; set; }
-    public string ImdbId { get; set; } = string.Empty;
-    public string? PlaceOfBirth { get; set; }
-    public double Popularity { get; set; }
-    public string? ProfilePath { get; set; }
+    public int Id { get; init; }
+    public string Name { get; init; }
+    public IReadOnlyList<string> AlsoKnownAs { get; init; }
+    public string KnownForDepartment { get; set; }
+    public bool Adult { get; init; }
+    public string Biography { get; init; }
+    public DateTime? Birthday { get; init; }
+    public DateTime? DeathDay { get; init; }
+    public Gender Gender { get; init; }
+    public string Homepage { get; init; }
+    public string ImdbId { get; init; }
+    public string PlaceOfBirth { get; init; }
+    public double Popularity { get; init; }
+    public string ProfilePath { get; init; }
 
     public override string ToString() => Name;
 }
