45a6c03ed74b33770fab697978e2a7ea1514c4e0
Mohammad Sadeq Sirjani <m.sadeq.sirjani@gmail.com>
2023-05-05 19:05:00 +0330

    feat(core-person-movie-credit): add person movie credit class

---
+++ src/WatchLister.Core/People/PersonMovieCredit.cs
@@ -0,0 +1,14 @@
+﻿namespace WatchLister.Core.People;
+
+public class PersonMovieCredit
+{
+    public PersonMovieCredit()
+    {
+        CastRoles = Array.Empty<PersonMovieCastMember>();
+        CrewRoles = Array.Empty<PersonMovieCrewMember>();
+    }
+
+    public int PersonId { get; init; }
+    public IReadOnlyList<PersonMovieCastMember> CastRoles { get; init; }
+    public IReadOnlyList<PersonMovieCrewMember> CrewRoles { get; init; }
+}
