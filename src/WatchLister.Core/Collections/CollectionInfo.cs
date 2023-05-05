df247b1b7ce885e4626accfc31fff83987c874f8
Mohammad Sadeq Sirjani <m.sadeq.sirjani@gmail.com>
2023-05-05 18:32:39 +0330

    feat(CollectionInfo.cs): add CollectionInfo class
    Add CollectionInfo class to Id, Name, PosterPath, and BackdropPath properties. Also, override ToString() method for readability.

--- src/WatchLister.Core/Collections/CollectionInfo.cs
+++ src/WatchLister.Core/Collections/CollectionInfo.cs
@@ -1,11 +1,11 @@
-namespace WatchLister.Core.Collections;
+﻿namespace WatchLister.Core.Collections;
 
 public class CollectionInfo
 {
     public int Id { get; init; }
     public string Name { get; init; } = string.Empty;
-    public string PosterPath { get; init; } = string.Empty;
-    public string BackdropPath { get; init; } = string.Empty;
+    public string? PosterPath { get; init; }
+    public string? BackdropPath { get; init; }
 
-    public override string ToString() => string.IsNullOrEmpty(Name) ? "n/a" : $"{Name} ({Id})";
+    public override string ToString() => string.IsNullOrWhiteSpace(Name) ? "n/a" : $"{Name} ({Id})";
 }
