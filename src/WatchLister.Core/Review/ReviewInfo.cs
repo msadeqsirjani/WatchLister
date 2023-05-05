7db0605614a592512b949006766624d63c451af6
Mohammad Sadeq Sirjani <m.sadeq.sirjani@gmail.com>
2023-05-05 19:07:52 +0330

    Create ReviewInfo.cs

---
+++ src/WatchLister.Core/Review/ReviewInfo.cs
@@ -0,0 +1,9 @@
+namespace WatchLister.Core.Review;
+
+public class ReviewInfo
+{
+    public string Author { get; init; } = string.Empty;
+    public string Content { get; init; } = string.Empty;
+    public string Id { get; init; } = string.Empty;
+    public string Url { get; init; } = string.Empty;
+}
