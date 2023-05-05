264a20364a241cd7e2523307d9e95070547cdbf3
Mohammad Sadeq Sirjani <m.sadeq.sirjani@gmail.com>
2023-05-05 18:56:25 +0330

    feat(core-movie-credit): add movie credit class

---
+++ src/WatchLister.Core/Movies/MovieCredit.cs
@@ -0,0 +1,8 @@
+﻿namespace WatchLister.Core.Movies;
+
+public class MovieCredit
+{
+    public int MovieId { get; init; }
+    public IReadOnlyList<MovieCastMember> CastMembers { get; init; } = new List<MovieCastMember>();
+    public IReadOnlyList<MovieCrewMember> CrewMembers { get; init; } = new List<MovieCrewMember>();
+}
