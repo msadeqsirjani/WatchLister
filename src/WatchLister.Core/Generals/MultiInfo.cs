c4b1b052c0d1426a3d2f254379cc888505045d0e
Mohammad Sadeq Sirjani <m.sadeq.sirjani@gmail.com>
2023-05-05 18:47:07 +0330

    feat(core-multi-info): add multi info class

---
+++ src/WatchLister.Core/Generals/MultiInfo.cs
@@ -0,0 +1,8 @@
+namespace WatchLister.Core.Generals;
+
+public class MultiInfo
+{
+    public int Id { get; init; }
+    public virtual MediaType MediaType { get; init; }
+    public double Popularity { get; init; }
+}
