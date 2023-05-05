3c6cccb836d59f9a94f224273d6035ac3a03b4b0
Mohammad Sadeq Sirjani <m.sadeq.sirjani@gmail.com>
2023-05-05 19:07:07 +0330

    Create PersonTvCredit.cs

---
+++ src/WatchLister.Core/People/PersonTvCredit.cs
@@ -0,0 +1,14 @@
+﻿namespace WatchLister.Core.People;
+
+public class PersonTvCredit
+{
+    public PersonTvCredit()
+    {
+        CastRoles = Array.Empty<PersonTvCastMember>();
+        CrewRoles = Array.Empty<PersonTvCrewMember>();
+    }
+
+    public int PersonId { get; init; }
+    public IReadOnlyList<PersonTvCastMember> CastRoles { get; init; }
+    public IReadOnlyList<PersonTvCrewMember> CrewRoles { get; init; }
+}
