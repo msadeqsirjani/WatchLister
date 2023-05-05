3c22859df63d890b43851eeae55dfa0cccae1168
Mohammad Sadeq Sirjani <m.sadeq.sirjani@gmail.com>
2023-05-05 18:44:09 +0330

    feat9core-external-ids): add external ids class

---
+++ src/WatchLister.Core/Generals/ExternalIds.cs
@@ -0,0 +1,9 @@
+namespace WatchLister.Core.Generals;
+
+public class ExternalIds
+{
+    public string FreebaseId { get; init; } = string.Empty;
+    public string FreebaseMid { get; init; } = string.Empty;
+    public int Id { get; init; }
+    public string TvrageId { get; init; } = string.Empty;
+}
