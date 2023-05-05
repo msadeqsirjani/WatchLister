e7a578be68d473f4b7f726ac35dfdb34b3fc6df4
Mohammad Sadeq Sirjani <m.sadeq.sirjani@gmail.com>
2023-05-05 19:47:01 +0330

    feat(core-tmdb-options): add tmdb option class

---
+++ src/WatchLister.Core/TmdbOptions.cs
@@ -0,0 +1,9 @@
+namespace WatchLister.Core;
+
+public class TmdbOptions
+{
+    public string BaseApiAddress { get; set; } = string.Empty;
+    public string ApiKey { get; set; } = string.Empty;
+    public string Language { get; set; } = string.Empty;
+    public string Region { get; set; } = string.Empty;
+}
